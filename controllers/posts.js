297fcef9ec1e01f006dfbd1bc4ab999de948eb69
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-08-05 16:11:52 -0400

    index and create route completed

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Posts = require('../models/posts.js')
 
+// index route
 router.get('/', (req, res) => {
-    res.send('index')
+    Posts.find({}, (err, foundPosts) => {
+        res.json(foundPosts)
+    })
+})
+
+// create route
+router.post('/', (req, res) => {
+    Posts.create(req.body, (err, createdPost) => {
+        res.json(createdPost)
+    })
 })
 
 module.exports = router
